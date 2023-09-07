bae461683c4edc60ad392ca61d03b3dbe244290b
Rebeca Lima <Rebecalmv.design@gmail.com>
2023-09-07 18:26:43 -0300

    teste de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    test('Deve renderizar o componente corretamente', () => {
+        render(<PostComment />)
+        expect(screen.getByText('Comentar')).toBeInTheDocument()
     });
+
+    test('Deve adicionar "Comentario 1" na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Comentario 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Comentario 1')).toBeInTheDocument()
+    })
+
+    test('Deve adicionar "Comentario 2" na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Comentario 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Comentario 2')).toBeInTheDocument()
+    })
 });
